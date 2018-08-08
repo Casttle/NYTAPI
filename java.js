b14e170945178ac4f3402d2a19decaa5af5a3c1a
Casttle <cberry1222@gmail.com>
2018-08-07 21:29:17 -0500

    update

--- java.js
+++ java.js
@@ -1,20 +1,21 @@
-var firstUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json"
-const key = "api-keyf1f5266670b64bfdaab0e26524e74684"
+var firstUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+const key = "api-keyf1f5266670b64bfdaab0e26524e74684";
 var search;
 var beginDate;
 var endDate;
 var pages;
 
+var url = "https//:api.nytimes.com/svc/search/v2/articlesearch.json?q=cats&api-key=f1f5266670b64bfdaab0e26524e74684";
 
-
-var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-url += '?' + $.param({
-  'api-key': "f1f5266670b64bfdaab0e26524e74684",
-  'q': "cats",
-  'begin_date': "20001112",
-  'end_date': "20100620",
-  'page': 5
-});
+// var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+// url += '?' + $.param({
+//   'api-key': "f1f5266670b64bfdaab0e26524e74684",
+//   'q': "cats",
+//   'begin_date': "20001112",
+//   'end_date': "20100620",
+//   'page': 5
+// });
+function search(){
 $.ajax({
   url: url,
   method: 'GET',
@@ -23,3 +24,36 @@ $.ajax({
 }).fail(function(err) {
   throw err;
 });
+
+
+
+$(".col").append(".form-group")
+}
+
+search();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$('#search').on('click', function(){console.log("frisbee")}) ;
+$('#search').on('click', function(){console.log("frisbee")}) ;
+
